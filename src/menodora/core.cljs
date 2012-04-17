da50ee8f50461105e2261c678832bfb1bddea5be
Jeremy Hughes <jedahu@gmail.com>
2012-04-18 03:38:54 +1200

    prepend text with "should "

--- src/menodora/core.cljs
+++ src/menodora/core.cljs
@@ -20,7 +20,7 @@
 (defn should*
   [text f]
   (fn [opts k]
-    (let [title [(:suite opts) (:descr opts) text]]
+    (let [title [(:suite opts) (:descr opts) (str "should " text)]]
       (r/-test-start @suite-runner title)
       (f (fn []
            (r/-test-end @suite-runner title)
