b0a81b86eea40380b8f15c6fdd25de323babaec6
Jeremy Hughes <jedahu@gmail.com>
2012-04-20 00:56:16 +1200

    menodora.server/serve-cljs: more options

--- src/menodora/server.clj
+++ src/menodora/server.clj
@@ -5,25 +5,29 @@
     [java.io File]))
 
 (defn- serve-cljs-
-  [{:keys [output-dir output-to] :as opts}]
+  [{:keys [output-dir output-to] :as build-opts}
+   & {:keys [test-uri handler] :as opts :or {test-uri "/"}}]
   (println "running jetty..." (pr-str opts))
   (jetty/run-jetty
     (fn [req]
       (cond
-        (= "/" (:uri req))
+        (= test-uri (:uri req))
         {:status 200
          :headers {"Content-Type" "text/html"}
          :body
          (str "<html><head><meta charset='UTF-8'/></head></body>"
               (when (.exists (File. (str output-dir "/goog/base.js")))
                 "<script src='"
-                (str output-dir "/goog/base/.js")
+                (str output-dir "/goog/base.js")
                 "'></script>")
-              "<script src='/" (:output-to opts) "'></script>")}
+              "<script src='/" output-to "'></script>")}
 
         (.exists (File. (str "." (:uri req))))
         {:status 200
          :body (slurp (str "." (:uri req)))}
+
+        handler
+        (handler req)
         
         :else
         {:status 404}))
