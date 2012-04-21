d72d854d656729e634ddf52866beeea6f3dff446
Jeremy Hughes <jedahu@gmail.com>
2012-04-22 00:59:59 +1200

    update to lein-cst 0.2.1

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
    [ring/ring-jetty-adapter "1.1.0-RC1"]]
 
   :plugins
-  [[lein-cst "0.1.0-SNAPSHOT"]]
+  [[lein-cst "0.2.1"]]
 
   :exclusions
   [org.apache.ant/ant]
@@ -18,29 +18,4 @@
   :source-path "src"
 
   :cst
-  {:builds
-   {:dev {:output-dir ".cst-out/dev"
-          :optimizations nil
-          :pretty-print true}
-    :single {:output-dir ".cst-out/single"
-             :optimizations :whitespace
-             :pretty-print true}
-    :small {:output-dir ".cst-out/small"
-            :optimizations :advanced
-            :pretty-print false}
-    :deploy {:output-to "menodora.js"
-             :output-dir ".cst-out/deploy"
-             :optimizations :advanced
-             :pretty-print false}}
-   :build :dev
-   :suites [menodora.test.core/core-tests]
-   :runners
-   {:rhino-console {:cljs menodora.runner.console/run-suites-rhino
-                    :proc :rhino
-                    :build :single}
-    :v8-console {:cljs menodora.runner.console/run-suites-v8
-                 :proc ["d8"]
-                 :build :single}
-    :browser-console {:cljs menodora.runner.console/run-suites-browser
-                      :proc cst.server/serve-cljs}}
-   :runner :rhino-console})
+  {:suites [menodora.test.core/core-tests]})
