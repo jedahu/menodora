dc2b41a7fc2aeca953761cc32fdf83b4ab753c52
Jeremy Hughes <jedahu@gmail.com>
2012-04-22 19:15:21 +1200

    add truthy and type-eq predicates

--- src/menodora/predicates.cljs
+++ src/menodora/predicates.cljs
@@ -4,3 +4,12 @@
   {:test =
    :message #(str "Expected: " (pr-str %1)
                   ". Actual: " (pr-str %2) ".")})
+
+(def truthy
+  {:test boolean
+   :message (constantly "Expected a truthy value.")})
+
+(def type-eq
+  {:test #(= %1 (type %2))
+   :message #(str "Expected type: " %1
+                  ". Actual type: " %2)})
